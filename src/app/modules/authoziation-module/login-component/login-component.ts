5bb8f5488199554d23f0025801067d51ec9f5533
Allan2Fernandes <allan2fernandes@hotmail.com>
2026-09-03 10:45:04 +0530

    WIP: Authorization

---
+++ src/app/modules/authoziation-module/login-component/login-component.ts
@@ -0,0 +1,31 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthorizationService } from '../../../services/authorization-service';
+import { TranslatePipe } from "../../../pipes/translate-pipe";
+import { Button } from "../../shared/button/button";
+
+@Component({
+  selector: 'app-login-component',
+  imports: [ReactiveFormsModule, TranslatePipe, Button],
+  templateUrl: './login-component.html',
+})
+export class LoginComponent implements OnInit {
+  fb = inject(FormBuilder);
+  authorizationService = inject(AuthorizationService);
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  });
+
+  ngOnInit(): void {
+    this.authorizationService.redirectIfAlreadyLoggedIn();
+  }
+
+  login() {
+    if(this.form.controls.email.value === null || this.form.controls.password.value === null) {
+      return;
+    }
+    this.authorizationService.login(this.form.controls.email.value, this.form.controls.password.value);
+  }
+}
---
+++ src/app/services/authorization-service.ts
@@ -0,0 +1,73 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { TokenService } from './token-service';
+import { Router } from '@angular/router';
+import { ToastService } from './toast-service';
+import { TranslationService } from './translation-service';
+import { Helper } from './helper';
+import { LoginResponse } from '../models/LoginResponse';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthorizationService {
+  httpClient = inject(HttpClient);
+  tokenService = inject(TokenService)
+  router = inject(Router);
+  toastService = inject(ToastService);
+  translateService = inject(TranslationService);
+
+  login(email: string, password: string){
+      this.httpClient.post<LoginResponse>('auth/login', {email: email, password: password})
+    .subscribe({
+      next: (res: LoginResponse) => {
+        this.tokenService.setToken(res.token);
+        this.router.navigate(['/fleet-viewer']);
+        this.toastService.add({
+          id: crypto.randomUUID(),
+          type: 'success',
+          message: this.translateService.translate("SUCCESSFULLY_LOGGED_IN")
+        })
+        
+      },
+      error: (err) => {
+        this.toastService.add({
+          id: crypto.randomUUID(),
+          type: 'error',
+          message: err.error.message
+        });
+      }
+    });
+  }
+
+  logout() {
+        this.httpClient.post('auth/logout', [])
+    .subscribe({
+      next: (res) => {
+        this.tokenService.clearToken();
+         this.toastService.add({
+          id: crypto.randomUUID(),
+          type: 'success',
+          message: this.translateService.translate("SUCCESSFULLY_LOGGED_OUT")
+        });
+        this.router.navigateByUrl('authorization/login');
+      },
+      error: (err) => {
+        this.toastService.add({
+          id: crypto.randomUUID(),
+          type: 'error',
+          message: err.error.message
+        });
+      }
+    });
+  }
+
+  redirectIfAlreadyLoggedIn() {
+    const token = this.tokenService.getToken();
+    if(Helper.isNullOrUndefined(token) || Helper.isEmptyString(token)) {
+      return;
+    }
+    
+    this.router.navigate(['/fleet-viewer']);
+  }
+}
