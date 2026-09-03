c6c73ec5f91f3a840a29329f871df004164cca74
Allan2Fernandes <allan2fernandes@hotmail.com>
2026-09-03 20:34:24 +0530

    attempt #3

--- src/app/services/robot-api-service.ts
+++ src/app/services/robot-api-service.ts
@@ -15,6 +15,6 @@ export class RobotApiService {
   }
 
   getListOfRobots(): Observable<Robot[]> {
-    return this.httpClient.post<Robot[]>('robots/', {});
+    return this.httpClient.get<Robot[]>('robots/get-all-robots',);
   }
 }
